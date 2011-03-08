e421ab173ec12f82292bb8d50a03d9c998470bb9
Julien Roncaglia <julien@roncaglia.fr>
2011-03-08 01:31:42 +0100

    Add some small test functions gathering statistics
    They are for now displayed in the page action but should be displayed
    along with the options instead.
    
    The display to be useful should also have a percentage display of
    secure / httpsOnly as it is the stat that need to be tracker.
    
    Once the filtering is implemented it may be needed to have the stats
    without the data that will disapear in a short time.

--- html/js/background.js
+++ html/js/background.js
@@ -66,3 +66,60 @@ function cookieTracker()
 console.log("Loading background...");
 window.tabs = new Tabs();
 console.log("Background loaded.");
+
+Y = function(f) {
+  return (function(g) {
+    return g(g);
+  })(function(h) {
+    return function() {
+      return f(h(h)).apply(null, arguments);
+    };
+  });
+};
+
+window.utils = {
+	findAllCookies: function(filters, callback) {
+		if (callback === undefined) {
+			callback = filters;
+			filters = undefined;
+		}
+
+		chrome.cookies.getAllCookieStores(function(stores) {
+			
+			var allCookies = [];
+
+			Y(function(recursion) {
+				return function(cookies) {
+					jQuery.merge(allCookies, cookies);
+					
+					if (stores.length > 0)
+					{
+						var nextStore = stores.pop();
+						chrome.cookies.getAll(jQuery.extend({ storeId: nextStore.id }, filters), recursion);
+					}
+					else
+					{
+						callback(allCookies);
+					}
+				};
+			})([]);
+		});
+	},
+	getCookiesStats: function(callback) {
+		window.utils.findAllCookies(function(cookies) {
+			var result = { count: cookies.length, persistent: 0, secure: 0, httpOnly: 0 };
+			jQuery.each(cookies, function(index, cookie) {
+				if (cookie.expirationDate) {
+					result.persistent = result.persistent + 1;
+				}
+				if (cookie.secure) {
+					result.secure = result.secure + 1;
+				}
+				if (cookie.httpOnly) {
+					result.httpOnly = result.httpOnly + 1;
+				}
+			});
+			callback(result);
+		});
+	}
+};
